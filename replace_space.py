42c943c98a7ea04ea5c0d672bec96cf87e886aaa
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:35:43 +0530

    Replace spaces from string data

---
+++ replace_space.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 11:07:47 2021
+
+@author: safis
+"""
+
+story="my name is    suraj"
+double_space=story.find("  ")
+
+print(double_space)
+story=story.replace("  ",' ')
+print(story)
